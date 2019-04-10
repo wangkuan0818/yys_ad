094792732927db8e7a7410e001295fd7f630c924
王宽 <648208376@qq.com>
2019-04-10 15:57:46 +0800

    .

--- client/src/pages/index/index.weapp.js
+++ client/src/pages/index/index.weapp.js
@@ -1,11 +1,18 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, } from '@tarojs/components'
-import { AtTag } from 'taro-ui'
+import { AtSteps, AtButton } from 'taro-ui'
 import './index.less'
 
+import CONFIG from '../../util/config'
 // import Login from '../../components/login/index.weapp'
 
 export default class Index extends Component {
+  constructor() {
+    super(...arguments)
+    this.state = {
+      stepCurrent: 0,
+    }
+  }
 
   config = {
     navigationBarTitleText: '首页'
@@ -21,12 +28,24 @@ export default class Index extends Component {
 
   componentDidHide () { }
 
+  stepOnChange = (current) => {
+    this.setState({
+      stepCurrent: current,
+    })
+  }
+
   render () {
+    let {stepCurrent} = this.state
+
     return (
       <View className='index'>
-        <AtTag>标签</AtTag>
-        <AtTag type='primary' circle>标签</AtTag>
-        <AtTag size='small'>标签</AtTag>
+        <AtSteps
+          items={CONFIG.stepItems}
+          current={stepCurrent}
+        />
+
+        <AtButton onClick={this.stepOnChange.bind(this,1)}>点击</AtButton>
+
         {/* 注释云服务 */}
         {/* <Login /> */}
       </View>
---
+++ client/src/util/config.js
@@ -0,0 +1,7 @@
+export default {
+    stepItems : [
+        { 'title': '步骤一', 'desc': '选择你提供的式神碎片', },
+        { 'title': '步骤二', 'desc': '选择需要换的式神碎片' },
+        { 'title': '步骤三', 'desc': '保存图片', 'icon': { value: 'check' } }
+    ]
+}
